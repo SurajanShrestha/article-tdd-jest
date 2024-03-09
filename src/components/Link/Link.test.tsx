c471601a30eb09b2bb19d2beb67f40c35ba8cbe5
Surajan Shrestha <surajanshrestha35@gmail.com>
2024-03-09 17:33:12 +0545

    chore: removed unwanted comments

--- src/components/Link/Link.test.tsx
+++ src/components/Link/Link.test.tsx
@@ -1,8 +1,6 @@
 import renderer from "react-test-renderer";
 import { Link } from ".";
 
-// Or you could use it() instead of test()
-// it() is an alias of test()
 test("link renders correctly", () => {
   const component = renderer.create(
     <Link page="http://www.instagram.com">Instagram</Link>
@@ -25,11 +23,6 @@ it("link changes when hovered", () => {
 
   // Re-rendering
   // In snapshot: "className" of <a> will be "hovered"
-  /* 
-    IMPORTANT 💡: If you change the code for STATUS.HOVERED from "hovered" to "hovering"(let's say), tests will fail.
-    This will let you keep track of your code, especially if another developer changes something in the component that
-    is shared across or used in many react pages.
-  */
   tree = component.toJSON() as renderer.ReactTestRendererJSON;
   expect(tree).toMatchSnapshot();
 
